75147caf7d9c500996298c781c694157389421b9
Nick Levshin <nikita.levshin2016@gmail.com>
2022-07-07 19:50:02 +0300

    fix: update url

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: 'ac98d50ee0fd43c2be200fd0af145bfa',
         });
     }
